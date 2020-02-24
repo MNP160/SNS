8999b47c39c97c34584f896855b518687b8c6a7a
Mladen <mpenev13@gmail.com>
2020-02-24 12:13:56 +0200

    Initial Commit

---
+++ SNS/SNS/Areas/Identity/Data/SNSUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SNS.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the SNSUser class
+    public class SNSUser : IdentityUser
+    {
+
+        public string Department { get; set; }
+    }
+}
---
+++ SNS/SNS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SNS.Areas.Identity.Data;
+using SNS.Data;
+
+[assembly: HostingStartup(typeof(SNS.Areas.Identity.IdentityHostingStartup))]
+namespace SNS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SNSContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SNSContextConnection")));
+
+                services.AddDefaultIdentity<SNSUser>()
+                    .AddEntityFrameworkStores<SNSContext>();
+            });
+        }
+    }
+}
